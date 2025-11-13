a17c4caceac5c3c4428de0b0ccbbf32bc617ac50
Akash <shridharshetty.a@northeastern.edu>
2025-11-12 22:26:21 -0500

    fix main

--- index.js
+++ index.js
@@ -1,44 +1,69 @@
+import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import session from "express-session";
+
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
-import cors from "cors";
-import db from "./Kambaz/Database/index.js";
+import PathParameters from "./Lab5/PathParameters.js";
+import QueryParameters from "./Lab5/QueryParameters.js";
+import WorkingWithObjects from "./Lab5/WorkingWithObjects.js";
+import WorkingWithArrays from "./Lab5/WorkingWithArrays.js";
 import UserRoutes from "./Kambaz/Users/routes.js";
 import CourseRoutes from "./Kambaz/Courses/routes.js";
 import ModulesRoutes from "./Kambaz/Modules/routes.js";
-import AssignmentsRoutes from "./Kambaz/Assignments/routes.js";
-import EnrollmentsRoutes from "./Kambaz/Enrollments/routes.js";
-import "dotenv/config";
-import session from "express-session";
+import AssignmentRoutes from "./Kambaz/Assignments/routes.js";
+import EnrollmentRoutes from "./Kambaz/Enrollments/routes.js";
+
+import db from "./Kambaz/Database/index.js";
 
 const app = express();
+
 app.use(
   cors({
     credentials: true,
     origin: process.env.CLIENT_URL || "http://localhost:3000",
   })
 );
+
+app.set("trust proxy", 1);
+
 const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kambaz",
   resave: false,
   saveUninitialized: false,
 };
-if (process.env.NODE_ENV === "production") {
+
+if (process.env.SERVER_ENV !== "development") {
   sessionOptions.proxy = true;
   sessionOptions.cookie = {
     sameSite: "none",
     secure: true,
-    domain: process.env.SERVER_URL,
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000,
+    domain: "kambaz-node-server-app-vy3q.onrender.com",
+    partitioned: true,
   };
 }
+
 app.use(session(sessionOptions));
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 UserRoutes(app, db);
 CourseRoutes(app, db);
 ModulesRoutes(app, db);
-AssignmentsRoutes(app, db);
-EnrollmentsRoutes(app, db);
+AssignmentRoutes(app, db);
+EnrollmentRoutes(app, db);
 Lab5(app);
 Hello(app);
-app.listen(process.env.PORT || 4000);
+PathParameters(app);
+QueryParameters(app);
+WorkingWithObjects(app);
+WorkingWithArrays(app);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
