4002b777cdd0d10a1a4e2b1a53accf2ef0b6304f
Akash <shridharshetty.a@northeastern.edu>
2025-11-27 00:43:26 -0500

    commit

--- index.js
+++ index.js
@@ -32,10 +32,10 @@ const sessionOptions = {
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.SERVER_ENV === "production", // true in production
+    secure: process.env.SERVER_ENV === "production", 
     httpOnly: true,
     sameSite: process.env.SERVER_ENV === "production" ? "none" : "lax",
-    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+    maxAge: 24 * 60 * 60 * 1000, 
   },
 };
 
